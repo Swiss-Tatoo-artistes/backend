ccc3bbcf10d799dff1c6a46ee9093afbbc6ca71f
ChrisAZ <91840732+Hababjiour@users.noreply.github.com>
2024-02-27 22:31:01 +0100

    Create controller for display all and specific canton

--- app/Http/Controllers/CantonController.php
+++ app/Http/Controllers/CantonController.php
@@ -3,47 +3,63 @@
 namespace App\Http\Controllers;
 
 use App\Models\Canton;
-use Illuminate\Http\Request;
 
 class CantonController extends Controller
 {
 
 
-    ///////////////////// ICI REPRENDRE
-
-
     //Display all the cantons
-    public function index(Request $request)
+    public function index($language_translate)
     {
         // Récupérer la langue spécifique à partir de la requête
-        $lang = $request->input('lang', 'fr'); // Par défaut, utilisez 'fr' si la langue n'est pas spécifiée
+        $lang = $language_translate;
 
         // Filtrer les cantons en fonction de la langue spécifique
         $cantons = Canton::with(['cantonTraductions' => function ($query) use ($lang) {
             $query->where('language_translate', $lang);
-        }])->get();
+        }])
+            ->get()
+            ->map(function ($canton) {
+                $cantonArray = $canton->toArray();
+                $cantonTraductions = $cantonArray['canton_traductions'][0] ?? null;
+                unset($cantonArray['canton_traductions']);
+                if ($cantonTraductions) {
+                    $cantonArray['language_translate'] = $cantonTraductions['language_translate'];
+                    $cantonArray['translated_name'] = $cantonTraductions['name'];
+                }
+                return $cantonArray;
+            });
 
         return response()->json(['cantons' => $cantons], 200);
     }
 
 
 
+     //Display a specific canton
+     public function show($language_translate, $name)
+     {
+         // Récupérer la langue spécifique à partir de la requête
+         $lang = $language_translate;
+ 
+         // Filtrer les cantons en fonction de la langue spécifique
+         $cantons = Canton::with(['cantonTraductions' => function ($query) use ($lang) {
+             $query->where('language_translate', $lang);
+         }])
+         ->where('name', $name)
+             ->get()
+             ->map(function ($canton) {
+                 $cantonArray = $canton->toArray();
+                 $cantonTraductions = $cantonArray['canton_traductions'][0] ?? null;
+                 unset($cantonArray['canton_traductions']);
+                 if ($cantonTraductions) {
+                     $cantonArray['language_translate'] = $cantonTraductions['language_translate'];
+                     $cantonArray['translated_name'] = $cantonTraductions['name'];
+                 }
+                 return $cantonArray;
+             });
+ 
+         return response()->json(['cantons' => $cantons], 200);
+     }
 
 
-
-
-
-    // Display a specific canton
-    public function show($name)
-    {
-
-        // Rechercher le canton par nom
-        $canton = Canton::where('name', $name)->first();
-
-        if ($canton) {
-            return response()->json(['canton' => $canton], 200);
-        } else {
-            return response()->json(['message' => 'Canton not found'], 404);
-        }
-    }
 }
--- routes/canton.php
+++ routes/canton.php
@@ -8,11 +8,11 @@ Route::controller(CantonController::class)->group(function (){
 
 
     // The Route to display all the cantons
-    Route::get('{language}/cantons', 'index');
+    Route::get('{language_translate}/cantons', 'index');
 
 
     // The Route to display a specific canton
-    Route::get('{language}/cantons/{name}', 'show');
+    Route::get('{language_translate}/cantons/{name}', 'show');
 
 
 });
